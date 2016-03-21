2ccd130bf433297ef77d269f9c50eed0fda1d3d9
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-03-21 10:21:50 -0400

    Added files via upload

--- anypromise.js
+++ anypromise.js
@@ -2,10 +2,10 @@
 
 Promise.any = function(promises = []) {
  return(new Promise(function(resolve, reject) {
-  let failed = 0
+  let errors = []
   for(let promise of promises) {
    promise.then(resolve).catch(function(error) {
-    failed = failed + 1
-    if(failed == promises.length) {
-     reject(error)
+    errors.push(error)
+    if(promises.length == errors.length) {
+     reject(errors)
 } }) } })) }

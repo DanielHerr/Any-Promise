72c1f66665c06f0e4957fdf74308b75cdba4d2ab
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-11-28 16:35:30 -0500

    Add Fulfillers to Function and Promise Constructor

--- betterpromises.js
+++ betterpromises.js
@@ -1,42 +1,57 @@
 "use strict"
 
+Promise.fulfillers = function() {
+ let resolve, reject, promise = new Promise(function(resolver, rejector) {
+  resolve = resolver, reject = rejector
+ })
+ return(Object.assign([ promise, resolve, reject ], { promise, resolve, reject }))
+}
+
+Promise = new Proxy(Promise, {
+ construct(target, inputs) {
+  if(inputs.length) {
+   return(Reflect.construct(target, inputs))
+  } else {
+   return(Promise.fulfillers())
+} } })
+
 Promise.any = function(promises = []) {
- return(new Promise(function(resolve, reject) {
-  let errors = []
-  let totalpromises = 0
-  for(let promise of promises) {
-   totalpromises = totalpromises + 1
-   Promise.resolve(promise).then(resolve).catch(function(error) {
-    errors.push(error)
-    if(errors.length == totalpromises) {
-     reject(errors)
-} }) } })) }
+ promises = Array.from(promises)
+ if(promises.length) {
+  return(new Promise(function(resolve, reject) {
+   let errors = []
+   for(let promise of promises) {
+    Promise.resolve(promise).then(resolve).catch(function(error) {
+     errors.push(error)
+     if(errors.length == promises.length) {
+      reject(errors)
+  } }) } }))
+ } else {
+  throw(Error("No iterable inputs."))
+} }
 
 Promise.some = function(promises = []) {
- return(new Promise(function(resolve, reject) {
-  let totalpromises = 0
-  let totalresults = 0
-  let totalerrors = 0
-  let results = []
-  let errors = []
-  for(let promise of promises) {
-   let currentpromise = totalpromises
-   totalpromises = totalpromises + 1
-   Promise.resolve(promise).then(function(result) {
-    results[currentpromise] = result
-    errors[currentpromise] = undefined
-    totalresults = totalresults + 1
-   }).catch(function(error) {
-    results[currentpromise] = undefined
-    errors[currentpromise] = error
-    totalerrors = totalerrors + 1
-   }).then(function() {
-    if(totalresults + totalerrors == totalpromises) {
-     if(totalerrors == totalpromises) {
+ promises = Array.from(promises)
+ if(promises.length) {
+  return(new Promise(function(resolve, reject) {
+   let results = [], errors = []
+   let totalresults = 0, totalerrors = 0
+   for(let currentpromise = 0; currentpromise < promises.length; currentpromise++) {
+    let promise = promises[currentpromise]
+    Promise.resolve(promise).then(function(result) {
+     results[currentpromise] = result
+     errors[currentpromise] = undefined
+     totalresults = totalresults + 1
+    }).catch(function(error) {
+     results[currentpromise] = undefined
+     errors[currentpromise] = error
+     totalerrors = totalerrors + 1
+    }).then(function() {
+     if(totalerrors == promises.length) {
       reject(errors)
-     } else {
-      let result = [ results, errors ]
-      result.results = results
-      result.errors = errors
-      resolve(result)
-} } }) } })) }
+     } else if(totalresults + totalerrors == promises.length) {
+      resolve(Object.assign([ results, errors ], { results, errors }))
+  } }) } }))
+ } else {
+  throw(Error("No iterable inputs."))
+} }
--- tests.js
+++ tests.js
@@ -47,7 +47,7 @@ test("some result properties", function(pass, fail) {
   if(result.results && result.errors && result[0] && result[1]) {
    pass()
   } else {
-   fail('result.keys() should be [ "results, "errors", "0", "1" ] but is' + result)
+   fail('result should be "{ results, errors, 0: results, 1: errors }" but is ' + result)
 } }) })
 
 test("some results", function(pass, fail) {
@@ -95,3 +95,43 @@ test("some results and errors", function(pass, fail) {
   } else {
    fail('results should be "something" and "undefined" but are ' + results)
 } }) })
+
+test("fulfillers properties", function() {
+ let result = Promise.fulfillers()
+ if((result.promise && result.resolve && result.reject && result.length == 3) == false) {
+  throw(
+   `result should be "{ promise, resolve, reject, 0: promise, 1: resolve, 2: reject }"
+   but is ` + result
+) } })
+
+test("fulfillers resolve", function(pass, fail) {
+ let result1 = Promise.fulfillers()
+ result1.promise.then(function(result2) {
+  if(result2 == 4) {
+   pass()
+  } else {
+   fail("result should be 4 but is " + result2)
+ } })
+ result1.resolve(4)
+})
+
+test("fulfillers reject", function(pass, fail) {
+ let result1 = Promise.fulfillers()
+ result1.promise.then(function(result2) {
+  fail("should reject but resolved with " + result2)
+ }).catch(function(error) {
+  if(error == 4) {
+   pass()
+  } else {
+   fail("error should be 4 but is" + error)
+ } })
+ result1.reject(4)
+})
+
+test("fulfillers constructor", function() {
+ let result = new Promise()
+ if((result.promise && result.resolve && result.reject && result.length == 3) == false) {
+  throw(
+   `result should be { promise, resolve, reject, 0: promise, 1: resolve, 2: reject }
+   but is ` + result
+) } })
